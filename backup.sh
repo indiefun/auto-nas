496554453bc5d03408b4ee07fdeedb0ab21fab2b
18810777688 <kevin_loo@live.cn>
2023-06-08 15:59:12 +0800

    update shell

--- backup.sh
+++ backup.sh
@@ -3,9 +3,21 @@
 cd "$(dirname "$0")"
 ISO_TIME=$(date +"%Y-%m-%d_%H-%M-%S")
 
+./status.sh
+
+read -p "Do you have stopped all containers? [y/N] (see status output above): " -n 1 -r CONFIRMED
+if ! [[ $CONFIRMED =~ ^[Yy]$ ]]; then
+    echo "Retry after stoped all containers, ABORTING..."
+    exit
+fi
+echo
+
 if [ -f backup.tar.gz ]; then
     echo "backup.tar.gz already exists, rename to backup.tar.gz.${ISO_TIME}..."
     mv backup.tar.gz backup.tar.gz.${ISO_TIME}
 fi
 
+echo "backup.tar.gz created, saving..."
 tar -zcpf backup.tar.gz --exclude=backup.tar.gz* ./*
+
+echo "backup.tar.gz saved!"
--- recover.sh
+++ recover.sh
@@ -2,9 +2,21 @@
 
 cd "$(dirname "$0")"
 
+./status.sh
+
+read -p "Do you have stopped all containers? [y/N] (see status output above): " -n 1 -r CONFIRMED
+if ! [[ $CONFIRMED =~ ^[Yy]$ ]]; then
+    echo "Retry after stoped all containers, ABORTING..."
+    exit
+fi
+echo
+
 if [ ! -f backup.tar.gz ]; then
     echo "backup.tar.gz not found, aborting..."
     exit 1
 fi
 
+echo "backup.tar.gz found, extracting..."
 tar -zxpf backup.tar.gz
+
+echo "backup.tar.gz extracted!"
