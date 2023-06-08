dcb8a93f5bf8c0ab7efae90c704158c7662278bb
18810777688 <kevin_loo@live.cn>
2023-06-08 11:33:12 +0800

    update clear.sh

--- jellyfin/clear.sh
+++ jellyfin/clear.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
+echo "Into $PWD..."
 
-echo "Removing cache, config, media in $PWD..."
-sudo rm -rf cache config media
+if [ -d cache ]; then
+    echo "Removing cache..."
+    rm -rf cache
+fi
+
+if [ -d config ]; then
+    echo "Removing config..."
+    rm -rf config
+fi
+
+if [ -d media ]; then
+    echo "Removing media..."
+    rm -rf media
+fi
+
+echo ""
+echo "Clear Done!"
--- nextcloud/clear.sh
+++ nextcloud/clear.sh
@@ -1,6 +1,22 @@
 #!/usr/bin/env bash
 
 cd "$(dirname "$0")"
+echo "Into $PWD..."
 
-echo "Removing postgres, redis, data in $PWD..."
-sudo rm -rf postgres redis data
+if [ -d postgres ]; then
+    echo "Removing postgres..."
+    rm -rf postgres
+fi
+
+if [ -d redis ]; then
+    echo "Removing redis..."
+    rm -rf redis
+fi
+
+if [ -d data ]; then
+    echo "Removing data..."
+    rm -rf data
+fi
+
+echo ""
+echo "Clear Done!"
